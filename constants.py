b6d951472839fe6560abacde307ca81b5d358802
guicbelon <guilherme.c.belon@gmail.com>
2023-11-03 15:09:57 -0300

    add: rl model

---
+++ constants.py
@@ -0,0 +1,12 @@
+# AMOUNT TO OPERATE:
+AMOUNT_TO_OPERATE = 15
+
+#STORAGE
+TRADE_KEYS=['retcode','deal','order','volume','price','bid','ask','comment','request_id','retcode_external','action','timestamp','order','symbol','volume','price','stoplimit','sl','tp','deviation','type','type_filling','type_time','expiration','comment','position','position_by']
+
+#COSTS USED IN BACKTEST
+DEFAULT_TRADING_COST = 0.005
+DEFAULT_SPREAD_COST  = 0.001
+
+#NUMBER OF POSSIBLE STATES IN MARKOV CHAINS
+NUM_OF_SCALES = 3
